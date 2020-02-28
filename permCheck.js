074d3af6a69afc371178b6d48e73ffdacb06a23e
Tagor Petrosian <p.tagor@gmail.com>
2020-02-28 19:24:53 +0200

    first commit

---
+++ permCheck.js
@@ -0,0 +1,14 @@
+function between(x, min, max) {
+  return x >= min && x <= max;
+}
+
+function solution(A) {
+  const seen = new Array(A.length).fill(false);
+
+  for (let value of A) {
+    if (seen[value - 1]) return 0;
+    if (!between(value - 1, 0, A.length - 1)) return 0;
+    seen[value - 1] = true;
+  }
+  return 1;
+}
