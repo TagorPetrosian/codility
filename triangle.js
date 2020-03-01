dba439789a11e13e5faf1322c048d0d4165d2e68
Tagor Petrosian <p.tagor@gmail.com>
2020-03-01 17:03:50 +0200

    a little tweak to triangle

--- triangle.js
+++ triangle.js
@@ -33,15 +33,14 @@ each element of array A is an integer within the range [−2,147,483,648..2,147,
  */
 
 function solution(A) {
-  let res = 0;
   if (A.length < 3) return 0;
 
   A.sort((a, b) => a - b);
   for (let i = 0; i < A.length - 2; i++) {
-    if (A[i] + A[i + 1] > A[i + 2]) res = 1;
+    if (A[i] + A[i + 1] > A[i + 2]) return 1;
   }
 
-  return res;
+  return 0;
 }
 
 console.log(solution([10, 2, 5, 1, 8, 20]));
