74d44c2bb5ec3669d47633235c1fad6490b15825
Tagor Petrosian <p.tagor@gmail.com>
2021-10-12 18:41:55 +0300

    add binarySearch

---
+++ src/binarySearch/binarySearch.js
@@ -0,0 +1,37 @@
+// iterative solution
+// function binarySearchIterative(sortedArray, seekElement) {
+//   let startIndex = 0;
+//   let endIndex = sortedArray.length - 1;
+
+//   while (startIndex <= endIndex) {
+//     const middleIndex = startIndex + Math.floor((endIndex - startIndex) / 2);
+
+//     if (sortedArray[middleIndex] == seekElement) {
+//       return middleIndex;
+//     } else if (sortedArray[middleIndex] < seekElement) {
+//       startIndex = middleIndex + 1;
+//     } else {
+//       endIndex = middleIndex - 1;
+//     }
+//   }
+
+//   return -1;
+// }
+
+// recursive solution
+function binarySearchRecursive(sortedArray, seekElement, left = 0, right = sortedArray.length - 1) {
+  while (left <= right) {
+    let mid = left + Math.floor((right - left) / 2);
+
+    if (sortedArray[mid] == seekElement) {
+      return mid;
+    } else if (sortedArray[mid] < seekElement) {
+      return binarySearchRecursive(sortedArray, seekElement, mid + 1, right);
+    } else {
+      return binarySearchRecursive(sortedArray, seekElement, left, mid - 1);
+    }
+  }
+}
+
+console.log('res', binarySearchRecursive([1, 5, 10, 12, 14, 17, 22, 100], 17));
+console.log('res', binarySearch([1, 5, 10, 12, 14, 17, 22, 100], 17));
