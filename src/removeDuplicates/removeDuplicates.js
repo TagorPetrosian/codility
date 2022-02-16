62bb3c72afda230cf39af67befaac499eb74f709
Tagor Petrosian <p.tagor@gmail.com>
2022-02-16 17:10:17 +0200

    removeDups impl

--- src/removeDuplicates/removeDuplicates.js
+++ src/removeDuplicates/removeDuplicates.js
@@ -4,18 +4,22 @@
  * @param {number[]} nums
  * @return {number}
  */
-const removeDuplicates = function (nums) {
-  if (nums.length < 2) return nums.length;
 
-  let i = 0;
-  for (let j = 1; j < nums.length; j++) {
-    if (nums[j] > nums[i]) {
-      nums[i + 1] = nums[j];
+const removeDuplicates = function (nums) {
+  const { length } = nums;
+  if (length < 2) return length;
+  let writeIndex = 0;
+  for (let i = 0; i < length; ) {
+    if (nums[i] === nums[i + 1]) {
+      i++;
+    } else {
+      [nums[i], nums[writeIndex]] = [nums[writeIndex], nums[i]];
+      i++;
+      writeIndex++;
     }
-    i++;
   }
-  console.log(nums);
-  return i++;
+  return writeIndex;
 };
 
-console.log(removeDuplicates([1, 1, 2]));
+console.log(removeDuplicates([1, 1, 1, 1, 2]));
+console.log(removeDuplicates([1, 1, 2, 3, 3]));
